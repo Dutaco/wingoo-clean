17c5ded83db6817ebce3e33b597bdcd8285b3cee
Dutaco <dutaco@gmail.com>
2025-08-01 20:42:27 +0200

    Fix SQLAlchemy URI for Render deployment

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your_secret_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_UR")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
